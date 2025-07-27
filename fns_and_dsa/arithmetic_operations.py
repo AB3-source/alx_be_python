231302963e996d4d097ecc74c9dd59a3f912f4fe
AB3-source <dinerohardi@gmail.com>
2025-07-27 18:30:50 +0100

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,6 +8,8 @@ def multiply(num1, num2):
     return num1 * num2
 
 def divide(num1, num2):
+    if num2 == 0:
+        raise ValueError("Cannot divide by zero.")
     return num1 / num2
 
 def perform_operation(num1, num2, operation):
